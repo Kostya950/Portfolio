f8155b688d668796ef9495aadf07efd096509222
Kostya Kovalchuk <Kostya950@gmail.com>
2016-06-16 12:47:30 +0300

    no message

--- lang/en.php
+++ lang/en.php
@@ -9,8 +9,8 @@
 return array (
     'hello' => 'Hello guys!',
      'about_me' =>'My name is Konstantin Kovalchuk and I am junior php developer. This is my small portfolio where I
-     want to show what I can do. Here you will find a link to my own project, which was implemented from the very
-     beginning and brought to the implementation and use. Also, here you can find some executed test tasks!',
+     want to show what I can do. Here you will find a link to my own project which was implemented from the very
+     beginning and brought to the implementation and use. Also here you can find some executed test tasks!',
     'goal' => 'I really want to work and grow up, together with a strong and ambitious team!',
     'own_project' => 'Own project:',
     'test_tasks' => 'Test tasks:'
