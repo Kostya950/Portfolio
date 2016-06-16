57f23e41987f95f5d417578176603e4e911e999f
Kostya Kovalchuk <Kostya950@gmail.com>
2016-06-16 16:10:39 +0300

    no message

---
+++ webroot/js/functions.php
@@ -0,0 +1,18 @@
+<script>
+    alert(1);
+    function fetch_select(name,value)
+    {
+        $.ajax({
+            type: 'post',
+            url: 'ajax_search_functions.php',
+            data: {
+                name, value
+            },
+            success: function (response) {
+                document.getElementById(name).innerHTML=response;
+            }
+        });
+    }
+</script>
+
+
